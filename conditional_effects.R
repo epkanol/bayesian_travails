c3581470ae75473c75b2c839698a49e576b357cc
Anders Sundelin <epkanol@gmail.com>
2023-02-08 16:05:18 +0100

    Add conditional effects functions for removals

--- conditional_effects.R
+++ conditional_effects.R
@@ -84,3 +84,91 @@ plot_logADD_by_logDUP <- function(model, someData, ftot, aTeam, aRepo) {
       ggtitle(paste0("Conditional effects of team ", aTeam, " in repo ", aRepo))
   )
 }
+
+condeffect_logCOMPLEX_by_logREMOVED <- function(model, someData, aTeam, aRepo) {
+  items <- length(someData[,1])
+  nd <- someData |> mutate(R=0, A=0, D=0, team=aTeam, repo=aRepo, C=seq(from=-2, to=4, length.out=items))
+  nd1 <- someData |> mutate(R=1, A=0, D=0, team=aTeam, repo=aRepo, C=seq(from=-2, to=4, length.out=items))
+  nd2 <- someData |> mutate(R=2, A=0, D=0, team=aTeam, repo=aRepo, C=seq(from=-2, to=4, length.out=items))
+  nd3 <- someData |> mutate(R=3, A=0, D=0, team=aTeam, repo=aRepo, C=seq(from=-2, to=4, length.out=items))
+  f <- fitted(model, newdata=nd, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd)
+  f1 <- fitted(model, newdata=nd1, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd1)
+  f2 <- fitted(model, newdata=nd2, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd2)
+  f3 <- fitted(model, newdata=nd3, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd3)
+  f$R <- "0"
+  f1$R <- "1"
+  f2$R <- "2"
+  f3$R <- "3"
+  ftot <- rbind(f, f1, f2, f3)
+  return(ftot)
+}
+
+plot_logCOMPLEX_by_logREMOVED <- function(model, someData, ftot, aTeam, aRepo) {
+  observed <- bind_cols(someData, model$criteria$loo$diagnostics) |> filter(team == aTeam, repo == aRepo) |> mutate(truncR=as.factor(round(R)))
+  return(ftot |> ggplot(aes(x=C)) +
+           geom_smooth(aes(y=Estimate, ymin=Q5.5, ymax=Q94.5, group=R, color=R), stat="identity", alpha=.25, linewidth=.5) +
+           geom_point(data=observed, aes(y=y, size = pareto_k, color=truncR), alpha=0.2) +
+           ggtitle(paste0("Conditional effects of team ", aTeam, " in repo ", aRepo))
+  )
+}
+
+condeffect_logREMOVED_by_logCOMPLEX <- function(model, someData, aTeam, aRepo) {
+  items <- length(someData[,1])
+  ndmin1 <- someData |> mutate(C=-1, A=0, D=0, team=aTeam, repo=aRepo, R=seq(from=-2, to=4, length.out=items))
+  nd <- someData |> mutate(C=0, A=0, D=0, team=aTeam, repo=aRepo, R=seq(from=-2, to=4, length.out=items))
+  nd1 <- someData |> mutate(C=1, A=0, D=0, team=aTeam, repo=aRepo, R=seq(from=-2, to=4, length.out=items))
+  nd2 <- someData |> mutate(C=2, A=0, D=0, team=aTeam, repo=aRepo, R=seq(from=-2, to=4, length.out=items))
+  nd3 <- someData |> mutate(C=3, A=0, D=0, team=aTeam, repo=aRepo, R=seq(from=-2, to=4, length.out=items))
+  fmin1 <- fitted(model, newdata=ndmin1, probs=c(.055, .945)) |> data.frame() |> bind_cols(ndmin1)
+  f <- fitted(model, newdata=nd, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd)
+  f1 <- fitted(model, newdata=nd1, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd1)
+  f2 <- fitted(model, newdata=nd2, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd2)
+  f3 <- fitted(model, newdata=nd3, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd3)
+  fmin1$C <- "-1"
+  f$C <- "0"
+  f1$C <- "1"
+  f2$C <- "2"
+  f3$C <- "3"
+  ftot <- rbind(fmin1, f, f1, f2, f3)
+  return(ftot)
+}
+
+plot_logREMOVED_by_logCOMPLEX <- function(model, someData, ftot, aTeam, aRepo) {
+  observed <- bind_cols(someData, model$criteria$loo$diagnostics) |> filter(team == aTeam, repo == aRepo)|> mutate(truncD=as.factor(round(D)))
+  return(ftot |> ggplot(aes(x=R)) +
+           geom_smooth(aes(y=Estimate, ymin=Q5.5, ymax=Q94.5, group=C, color=C), stat="identity", alpha=.25, linewidth=.5) +
+           geom_point(data=observed, aes(y=y, size = pareto_k, color=truncD), alpha=0.2) +
+           ggtitle(paste0("Conditional effects of team ", aTeam, " in repo ", aRepo))
+  )
+}
+
+condeffect_logREMOVED_by_logDUP <- function(model, someData, aTeam, aRepo) {
+  items <- length(someData[,1])
+  ndmin1 <- someData |> mutate(D=-1, A=0, C=0, team=aTeam, repo=aRepo, R=seq(from=-2, to=4, length.out=items))
+  nd <- someData |> mutate(D=0, A=0, C=0, team=aTeam, repo=aRepo, R=seq(from=-2, to=4, length.out=items))
+  nd1 <- someData |> mutate(D=1, A=0, C=0, team=aTeam, repo=aRepo, R=seq(from=-2, to=4, length.out=items))
+  nd2 <- someData |> mutate(D=2, A=0, C=0, team=aTeam, repo=aRepo, R=seq(from=-2, to=4, length.out=items))
+  nd3 <- someData |> mutate(D=3, A=0, C=0, team=aTeam, repo=aRepo, R=seq(from=-2, to=4, length.out=items))
+  fmin1 <- fitted(model, newdata=ndmin1, probs=c(.055, .945)) |> data.frame() |> bind_cols(ndmin1)
+  f <- fitted(model, newdata=nd, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd)
+  f1 <- fitted(model, newdata=nd1, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd1)
+  f2 <- fitted(model, newdata=nd2, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd2)
+  f3 <- fitted(model, newdata=nd3, probs=c(.055, .945)) |> data.frame() |> bind_cols(nd3)
+  fmin1$D <- "-1"
+  f$D <- "0"
+  f1$D <- "1"
+  f2$D <- "2"
+  f3$D <- "3"
+  ftot <- rbind(fmin1, f, f1, f2, f3)
+  return(ftot)
+}
+
+plot_logREMOVED_by_logDUP <- function(model, someData, ftot, aTeam, aRepo) {
+  observed <- bind_cols(someData, model$criteria$loo$diagnostics) |> filter(team == aTeam, repo == aRepo) |> mutate(truncD=as.factor(round(D)))
+  return(ftot |> ggplot(aes(x=R)) +
+           geom_smooth(aes(y=Estimate, ymin=Q5.5, ymax=Q94.5, group=D, color=D), stat="identity", alpha=.25, linewidth=.5) +
+           geom_point(data=observed, aes(y=y, size = pareto_k, color=truncD), alpha=0.2) +
+           ggtitle(paste0("Conditional effects of team ", aTeam, " in repo ", aRepo))
+  )
+}
+
